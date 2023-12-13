e657609f9da1b0f1c763a4c5b1c6a63db62076e7
tsym_tima <tstimurb0311@gmail.com>
2023-12-13 23:26:17 +0700

    Пробую сохранить весь проект

---
+++ app/public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+//try {
+//    $pdo = new PDO("pgsql:host=db;port=5432;dbname=testdb;",  "testuser",  "qwerty");
+//
+//    print_r($pdo);
+//} catch (PDOException $throwable){
+//    echo $throwable->getMessage();
+//}
+//echo "\n" . "Hello, world!";
+//echo phpinfo();
--- app/public/post_registrate.php
+++ app/public/post_registrate.php
@@ -1,1 +1,12 @@
 <?php
+
+print_r($_GET);
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$password = $_GET['psw'];
+
+$pdo = new PDO("pgsql:host=db;port=5432;dbname=testdb;",  "testuser",  "qwerty");
+
+$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
+
