131b82cc49b60684ef8d28372fa92ecad3b067a7
tsym_tima <tstimurb0311@gmail.com>
2023-12-15 02:31:55 +0700

    Установил расширение xdebug, сделал возврат ошибочного заполения данных, пересмотрел конфигурацию PHP и Докера

--- app/public/get_registrate.php
+++ app/public/get_registrate.php
@@ -1,3 +1,4 @@
+<html>
 <form action="post_registrate.php" method="POST">
     <div class="container">
         <h1>Register</h1>
@@ -5,15 +6,19 @@
         <hr>
 
         <label for="name"><b>Name</b></label>
+        <label  style="color: deeppink"> <?php echo $errors['name'] ?? ''; ?> </label>
         <input type="text" placeholder="Enter Name" name="name" id="name" required>
 
         <label for="email"><b>Email</b></label>
+        <label style="color: deeppink"> <?php echo $errors['email'] ?? ''; ?> </label>
         <input type="text" placeholder="Enter Email" name="email" id="email" required>
 
         <label for="psw"><b>Password</b></label>
+        <label style="color: deeppink"> <?php echo $errors['password'] ?? ''; ?> </label>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
         <label for="psw-repeat"><b>Repeat Password</b></label>
+        <label style="color: deeppink"> <?php echo $errors['pswRepeat'] ?? ''; ?> </label>
         <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
         <hr>
 
@@ -25,6 +30,7 @@
         <p>Already have an account? <a href="#">Sign in</a>.</p>
     </div>
 </form>
+</html>
 
 <style>
 
@@ -32,12 +38,12 @@
 
     /* Add padding to containers */
     .container {
-    padding: 16px;
+        padding: 16px;
     }
 
     /* Full-width input fields */
     input[type=text], input[type=password] {
-width: 100%;
+        width: 100%;
         padding: 15px;
         margin: 5px 0 22px 0;
         display: inline-block;
@@ -46,19 +52,19 @@ width: 100%;
     }
 
     input[type=text]:focus, input[type=password]:focus {
-    background-color: #ddd;
+        background-color: #ddd;
         outline: none;
     }
 
     /* Overwrite default styles of hr */
     hr {
-    border: 1px solid #f1f1f1;
+        border: 1px solid #f1f1f1;
         margin-bottom: 25px;
     }
 
     /* Set a style for the submit/register button */
     .registerbtn {
-    background-color: #04AA6D;
+        background-color: #04AA6D;
         color: white;
         padding: 16px 20px;
         margin: 8px 0;
@@ -69,17 +75,17 @@ width: 100%;
     }
 
     .registerbtn:hover {
-    opacity:1;
-}
+        opacity:1;
+    }
 
     /* Add a blue text color to links */
     a {
-    color: dodgerblue;
-}
+        color: dodgerblue;
+    }
 
     /* Set a grey background color and center the text of the "sign in" section */
     .signin {
-    background-color: #f1f1f1;
+        background-color: #f1f1f1;
         text-align: center;
     }
 
--- app/public/post_registrate.php
+++ app/public/post_registrate.php
@@ -2,40 +2,35 @@
 
 //print_r($_POST);
 
-$Errors = [];      //создаем массив для ошибок
+$errors = [];      //создаем массив для ошибок
 
 $name = $_POST['name'];
-if (strlen($name) < 2) {        //проверка имени на количество символов
-    $Errors['name'] = "!Name must be more than 2 symbols!";
-}
-
 $email = $_POST['email'];
-if (strlen($email) < 2) {       //проверка почты на количество символов
-    $Errors['email'] = "!Email must be more than 2 symbols!";
-}
-
 $password = $_POST['psw'];
-if (strlen($password) < 7) {        //проверка пароля на количество символов
-    $Errors['password'] = "!Password must be more than 8 symbols!";
-}
-
 $pswRepeat = $_POST['psw-repeat'];
-if ($pswRepeat !== $password) {     //проверка на корректность пароля
-    $Errors['pswRepeat'] = "!Passwords don't match!";
+if (strlen($name) < 2) {        //проверка имени на количество символов
+    $errors['name'] = "!Name must be more than 2 symbols!";
+}
+if (strlen($email) < 2) {       //проверка почты на количество символов
+    $errors['email'] = "!Email must be more than 2 symbols!";
+}
+if (strlen($password) < 7) {        //проверка пароля на количество символов
+    $errors['password'] = "!Password must be more than 8 symbols!";
+} elseif ($pswRepeat !== $password) {     //проверка на корректность пароля
+    $errors['pswRepeat'] = "!Passwords don't match!";
 }
 
-print_r($Errors);
+print_r($errors);
 
-if (empty($Errors)) {
+if (empty($errors)) {
 //Connect with Data Base
-    $pdo = new PDO("pgsql:host=db;port=5432;dbname=testdb;",  "testuser",  "qwerty");
+    $pdo = new PDO("pgsql:host=db;port=5432;dbname=postgres;",  "postgres",  "postgres");
 
 //Подготавливаем наш запрос, экранируя в нем значения. Подготовка запроса идет только для записи в таблицу.
 //Если нужно просто выполнить команду, то exec
     $statement = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
 //выполнение запроса без возврата данных с помощью команды execute
     $statement->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-
 //Возвращаем только что заполненные данные на экран
     $statement = $pdo->prepare("SELECT * FROM users WHERE name = :name");
     $statement->execute(['name' => $name]);
@@ -43,3 +38,8 @@ if (empty($Errors)) {
 
     print_r($data);
 }
+
+require_once("get_registrate.php");
+
+?>
+
